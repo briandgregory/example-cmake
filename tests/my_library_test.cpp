5bfcfb5ca965156243396e375f2adf7d59d65725
Brian Gregory <briangregory@gmail.com>
2024-12-18 12:07:02 -0500

    add failing test

--- tests/my_library_test.cpp
+++ tests/my_library_test.cpp
@@ -6,6 +6,10 @@ TEST(MyLibraryTest, AddTest) {
     EXPECT_EQ(add(-1, 1), 0);
 }
 
+TEST(MyLibraryTest, FailTest) {
+    EXPECT_EQ(add(2,3),6);
+}
+
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
